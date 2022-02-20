961a366ae2944bc0f3bdae88b0c88957a672f2d0
Samuel <kungusamuel64@gmail.com>
2022-02-20 18:14:57 +0300

    Geolocation API

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Maps from './components/maps/Maps';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <List />
+      {/* <Header /> */}
+      {/* <List /> */}
       <Maps />
     </div>
   );
