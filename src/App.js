e0fd089d6e2c30991d7c66bc195dc2652bdca307
Samuel <kungusamuel64@gmail.com>
2022-02-20 17:29:14 +0300

    Added the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/header/Header';
+import List from './components/list/List';
+import Maps from './components/maps/Maps';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <List />
+      <Maps />
     </div>
   );
 }
