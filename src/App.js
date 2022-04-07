caed3f868ac5df6c337dbacb7dc9aef48f5ba378
Samuel <kungusamuel64@gmail.com>
2022-04-07 15:40:34 +0300

    Alan AI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+import React, { useEffect } from 'react'
+
+const alanKey = 'd7992c199e46e6d7a0b4a1376eaf77602e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+    useEffect(() => {
+       alanBtn({
+           key: alanKey,
+       })
+    }, [])
+    
   return (
     <div>
         <h1>
