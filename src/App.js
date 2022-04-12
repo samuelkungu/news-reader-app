29df815b5a2167a799433c004c1b002bb077d14b
Samuel <kungusamuel64@gmail.com>
2022-04-12 19:42:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
-import alanBtn from '@alan-ai/alan-sdk-web'
-import React, { useEffect, useState } from 'react'
-import NewsCards from './Components/NewsCards/NewsCards';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-const alanKey = 'd7992c199e46e6d7a0b4a1376eaf77602e956eca572e1d8b807a3e2338fdd0dc/stage';
+import logo from './images/logo.png';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
 
 const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
-     const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
 
-    useEffect(() => {
+  useEffect(() => {
        alanBtn({
-           key: alanKey,
-           onCommand: (command, articles) => {
-                if(command === 'newsHeadlines'){
-                    setNewsArticles(articles);
-                }
-           }
-       })
-    }, [])
+           key: 'd7992c199e46e6d7a0b4a1376eaf77602e956eca572e1d8b807a3e2338fdd0dc/stage',
+           onCommand: ({ command, articles, number }) => {
+            if (command === 'newHeadlines') {
+              setNewsArticles(articles);
+              setActiveArticle(-1);
+            } else if (command === 'instructions') {
+              setIsOpen(true);
+            } else if (command === 'highlight') {
+              setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+            } else if (command === 'open') {
+              const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+              const article = articles[parsedNumber - 1];
     
-  return (
-    <div>
-        <h1>
-            Alan AI news application
-            <NewsCards articles={newsArticles} />
-        </h1>
-    </div>
-  )
-}
-
-export default App
+              if (parsedNumber > articles.length) {
+                alanBtn().playText('Please try that again...');
+              } else if (article) {
+                window.open(article.url, '_blank');
+                alanBtn().playText('Opening...');
+              } else {
+                alanBtn().playText('Please try that again...');
+              }
+            }
+          },
+        });
+      }, []);
+    
+    return (
+      <div>
+        <div className={classes.logoContainer}>
+          {newsArticles.length ? (
+            <div className={classes.infoContainer}>
+              <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+              <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+            </div>
+          ) : null}
+          <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+        </div>
+        <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+        {!newsArticles.length ? (
+          <div className={classes.footer}>
+            <Typography variant="body1" component="h2">
+              Created by
+              SK -
+              Samuel Kung'u 
+            </Typography>
+            <img className={classes.image} src={logo} height="50px" alt="sk" />
+          </div>
+        ) : null}
+      </div>
+    );
+  };
+  
+  export default App;
