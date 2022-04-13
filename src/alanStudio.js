80a3fafea34398829a3eb407f39e1b9ba43acd15
Samuel <kungusamuel64@gmail.com>
2022-04-13 11:51:09 +0300

    Alan AI Commands

---
+++ src/alanStudio.js
@@ -0,0 +1,31 @@
+intent('What does this app do?', 'What can I do here?', 
+      reply('This is a news project.'));
+
+const API_KEY = '7bdfb1b10aca41c6becea47611b7c35a';
+let savedArticles = [];
+
+// News by Source
+intent('Give me the news from $(source* (.*))', (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
+    
+    if(p.source.value) {
+        NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
+    }
+    
+    api.request(NEWS_API_URL, (error, response, body) => {
+        const { articles } = JSON.parse(body);
+        
+        if(!articles.length) {
+            p.play('Sorry, please try searching for news from a different source');
+            return;
+        }
+        
+        savedArticles = articles;
+        
+        p.play({ command: 'newHeadlines', articles });
+        p.play(`Here are the (latest|recent) ${p.source.value}.`);
+  
+        p.play('Would you like me to read the headlines?');
+        p.then(confirmation);
+    });
+})
